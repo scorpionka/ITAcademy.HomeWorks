df03dad40d3a2e80252453c50a20daec54356142
Алексей Кузьмицкий <a.kuzmitsky@gmail.com>
2020-03-02 22:26:13 +0300

    commit2

--- Solution/HW.02/Program.cs
+++ Solution/HW.02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace HW._02
 {
@@ -14,6 +15,17 @@ namespace HW._02
             //6. Заускаем цикл, в котором конвертируем исходный код
             //7. В том же цикле сохраняем новый код в массиве
             //8. Записываем полученный код в файл картинки в папке Temp
+            StreamReader textReader = new StreamReader(@"C:\Temp\image.txt", true);
+            string textReaderResult = textReader.ReadToEnd();
+            textReader.Dispose();
+            string[] arrayOfTextResult = textReaderResult.Split(' ');
+            byte[] imageBytes = new byte[arrayOfTextResult.Length - 1];
+            for (int i = 0; i < arrayOfTextResult.Length - 1; i++)
+            {
+                byte binary = Convert.ToByte(arrayOfTextResult[i], 2);
+                imageBytes[i] = binary;
+            }
+            File.WriteAllBytes(@"C:\Temp\image.png", imageBytes);
         }
     }
 }
