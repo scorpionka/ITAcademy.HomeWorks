f9b7657d7c702dc4db3845c3f69a438ea800dc26
Алексей Кузьмицкий <scorpion_ka@outlook.com>
2020-03-19 04:26:00 +0300

    commit2

---
+++ Solution/HW_05.Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HW_05.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = Console.ReadKey().KeyChar;
+            Console.Clear();
+            switch (a)
+            {
+                case 'w':
+                    Console.WriteLine("Move forward!");
+                    break;
+                case 's':
+                    Console.WriteLine("Move back!");
+                    break;
+                case 'a':
+                    Console.WriteLine("Move left!");
+                    break;
+                case 'd':
+                    Console.WriteLine("Move right!");
+                    break;
+                default:
+                    Console.WriteLine("Dont't move!");
+                    break;
+            }
+        }
+    }
+}
