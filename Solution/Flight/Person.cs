d8fe1f927320fec93ae495cf5ed7ee6953a846a6
Алексей Кузьмицкий <scorpion_ka@outlook.com>
2020-04-10 03:38:14 +0300

    commit

--- Solution/Flight/Person.cs
+++ Solution/Flight/Person.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Reflection.PortableExecutable;
 
 namespace Flight
 {
@@ -43,26 +42,31 @@ namespace Flight
 
         public Person(string lastName, string firstName, string patronymic, string passportData, string flightNumber)
         {
-            if (!firstName.Equals(null))
+            try
             {
                 FirstName = Char.ToUpper(firstName[0]) + firstName.Substring(1);
             }
-            if (!lastName.Equals(null))
+            catch { }
+            try
             {
                 LastName = Char.ToUpper(lastName[0]) + lastName.Substring(1);
             }
-            if (!patronymic.Equals(null))
+            catch { }
+            try
             {
                 Patronymic = Char.ToUpper(patronymic[0]) + patronymic.Substring(1);
             }
-            if (!passportData.Equals(null))
+            catch { }
+            try
             {
                 PassportData = passportData.ToUpper();
             }
-            if (!flightNumber.Equals(null))
+            catch { }
+            try
             {
                 FlightNumber = flightNumber.ToUpper();
             }
+            catch { }
         }
         public void Greeting()
         {
@@ -90,21 +94,42 @@ namespace Flight
         }
         public void GetFullData(ref Person person)
         {
-            Console.WriteLine("Please, introduce yourself! Last name, first name, patronymic, your passport and flight details:");
+            Console.WriteLine("Please, introduce yourself! Last name, first name, patronymic, passport details and flight number:");
             string fullData = Console.ReadLine();
-            string[] fullDataSplit = new string[5];
-            fullDataSplit = fullData.Split();
-            while (fullDataSplit.Length < 2 || fullDataSplit.Length > 5)
+            string[] fullDataSplit = fullData.Split();
+            while (fullDataSplit.Length < 4 || fullDataSplit.Length > 5)
             {
                 Console.WriteLine("Wrong data! Try again!");
                 fullData = Console.ReadLine();
                 fullDataSplit = fullData.Split();
             }
+            if (fullDataSplit.Length.Equals(4))
+            {
+                Array.Resize(ref fullDataSplit, 5);
+                fullDataSplit[4] = fullDataSplit[3];
+                fullDataSplit[3] = fullDataSplit[2];
+                fullDataSplit[2] = defaultField;
+            }
             person = new Person(fullDataSplit[0], fullDataSplit[1], fullDataSplit[2], fullDataSplit[3], fullDataSplit[4]);
         }
         public void PrintAll()
         {
+            if (!Patronymic.Equals(defaultField))
+            {
+                PrintData(LastName, FirstName, Patronymic, PassportData, FlightNumber);
+            }
+            else
+            {
+                PrintData(LastName, FirstName, PassportData, FlightNumber);
+            }
+        }
+        private void PrintData(string lastName, string firstName, string patronymic, string passportData, string flightNumber)
+        {
             Console.WriteLine(LastName + " " + FirstName + " " + Patronymic + " " + PassportData + " " + FlightNumber);
+        }
+        private void PrintData(string lastName, string firstName, string passportData, string flightNumber)
+        {
+            Console.WriteLine(LastName + " " + FirstName + " " + PassportData + " " + FlightNumber);
         }
         public void PersonAccost()
         {
