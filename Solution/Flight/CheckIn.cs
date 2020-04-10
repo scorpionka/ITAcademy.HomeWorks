6c8555f1218dbbb706ed080e27c4391264ae46b3
Алексей Кузьмицкий <scorpion_ka@outlook.com>
2020-04-10 04:45:30 +0300

    commit

--- Solution/Flight/CheckIn.cs
+++ Solution/Flight/CheckIn.cs
@@ -6,5 +6,10 @@ namespace Flight
 {
     class CheckIn: Airport
     {
+
+        public void CheckRegistration()
+        {
+
+        }
     }
 }
--- Solution/Flight/Program.cs
+++ Solution/Flight/Program.cs
@@ -6,12 +6,13 @@ namespace Flight
     {
         static void Main(string[] args)
         {
-            Person newPerson = new Person();
+            Person newPerson = new Person("kuzmitski", "aliaksei", "vladimirovich", "mp3634182", "riga140320");
             newPerson.Greeting();
-            newPerson.GetFullData(ref newPerson);
+            //newPerson.GetFullData(ref newPerson);
             newPerson.PersonAccost();
             Console.WriteLine("Check your personal info!");
             newPerson.PrintAll();
+            Console.WriteLine(newPerson.Accost + "! Please, follow to the registration desk.");
 
         }
     }
