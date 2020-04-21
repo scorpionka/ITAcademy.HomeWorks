d930f6ad3c87bbce27b81fad5fb3c8cc09351c86
Алексей Кузьмицкий <scorpion_ka@outlook.com>
2020-04-21 04:18:38 +0300

    commit

---
+++ Solution/HW_12/IMotorcycleRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_12
+{
+    public interface IMotorcycleRepository : IDisposable
+    {
+        IEnumerable<Motorcycle> GetMotorcycles();
+        Motorcycle GetMotorcycleByID(int motorcycleID);
+        void CreateMotorcycle(Motorcycle motorcycle);
+        void DeleteMotorcycle(int motorcycleID);
+        void UpdateMotorcycle(Motorcycle motorcycle);
+        void Save();
+    }
+}
---
+++ Solution/HW_12/MotorcycleRepository.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW_12
+{
+    public class MotorcycleRepository : IMotorcycleRepository, IDisposable
+    {
+        private MotoContext context;
+
+        public MotorcycleRepository(MotoContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Motorcycle> GetStudents()
+        {
+            return context.Motorcycles.ToList();
+        }
+
+        public Motorcycle GetMotorcycleByID(int id)
+        {
+            return context.Motorcycles.Find(id);
+        }
+
+        public void InsertMotorcycle(Motorcycle student)
+        {
+            context.Motorcycles.Add(student);
+        }
+
+        public void DeleteMotorcycle(int motorcycleID)
+        {
+            Motorcycle motorcycle = context.Motorcycles.Find(motorcycleID);
+            context.Motorcycles.Remove(motorcycle);
+        }
+
+        public void UpdateMotorcycle(Motorcycle motorcycle)
+        {
+            context.Entry(motorcycle).State = EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
