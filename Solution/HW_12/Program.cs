8c24084aa0390e58d358730839efc3b19d11b444
Алексей Кузьмицкий <scorpion_ka@outlook.com>
2020-04-23 06:59:38 +0300

    commit

--- Solution/HW_12/Program.cs
+++ Solution/HW_12/Program.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using log4net;
+using log4net.Config;
+using System;
+
+[assembly: XmlConfigurator(ConfigFile = "app.config", Watch = true)]
 
 namespace HW_12
 {
     class Program
     {
+        public static readonly ILog log = LogManager.GetLogger(typeof(Program));
         static void Main(string[] args)
         {
+            log.Info("Program start!");
             MotorcycleRepository motorcycles = new MotorcycleRepository();
+            log.Info("Repository created!");
             motorcycles.PrintMotorcycleRepository(motorcycles);
             Motorcycle newMotorcycle = new Motorcycle()
             {
@@ -17,15 +24,18 @@ namespace HW_12
                 Year = 2020
             };
             motorcycles.CreateMotorcycle(newMotorcycle);
+            log.Info("New motorcycle information added!");
             Console.WriteLine("\n");
             motorcycles.PrintMotorcycleRepository(motorcycles);
             Motorcycle getIdTest = motorcycles.GetMotorcycleByID(3);
             Motorcycle getIdTestNull = motorcycles.GetMotorcycleByID(10);
+            log.Info("Motorcycle information received!");
             Console.WriteLine("\n");
             getIdTest.PrintMotorcycleInfo(getIdTest);
             getIdTestNull.PrintMotorcycleInfo(getIdTestNull);
             motorcycles.DeleteMotorcycle(2);
             motorcycles.DeleteMotorcycle(15);
+            log.Info("Motocycle information deleted!");
             Console.WriteLine("\n");
             motorcycles.PrintMotorcycleRepository(motorcycles);
             Motorcycle updateTest = new Motorcycle()
@@ -37,8 +47,10 @@ namespace HW_12
                 Year = 2019
             };
             motorcycles.UpdateMotorcycle(updateTest);
+            log.Info("Motocycle information updated!");
             Console.WriteLine("\n");
             motorcycles.PrintMotorcycleRepository(motorcycles);
+            log.Info("The program is completed!");
         }
     }
 }
